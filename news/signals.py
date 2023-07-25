b75ddf2553c023443a2d9c6b4925299ad744b109
Uchqun Usmonov <99597828+uchqunusmonov@users.noreply.github.com>
2023-07-25 22:55:46 +0500

    Update signals.py

--- news/signals.py
+++ news/signals.py
@@ -1,15 +1,38 @@
+import os
 import asyncio
-from django.db.models.signals import post_save
+from django.conf import settings
 from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 from .models import Post
-from .api.v1.telegram_bot import post_news_to_channel
+from .api.v1.telegram_bot import send_picture_and_link
+
+
+def run_async_in_thread(func, *args, **kwargs):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    task = asyncio.ensure_future(func(*args, **kwargs))
+    loop.run_until_complete(task)
+    loop.close()
 
 
 @receiver(post_save, sender=Post)
-def post_to_telegram(sender, instance, created, **kwargs):
+def post_to_telegram_channel(sender, instance, created, **kwargs):
     if created:
-        photo = instance.image.path
-        title = instance.title
-        link = instance.get_absolute_url()
+        channel_id = "@uchquns_blog"
 
-        asyncio.run(post_news_to_channel(title, link, photo))
+        picture_url = 'https://th.bing.com/th/id/R.6af6fd9c37f0de4abb34ea0fd20acce3?rik=55mqMmrTutVR0Q&pid=ImgRaw&r=0'
+
+        link_url = f"http://52.57.75.104{instance.get_absolute_url()}"
+
+        link_text = instance.body[:50]
+        
+        run_async_in_thread(send_picture_and_link, channel_id, picture_url, link_text, link_url)
+
+
+def post_to_telegram_async(*args, **kwargs):
+    # This function will be used to call the asynchronous signal handling function
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(post_to_telegram_channel(*args, **kwargs))
+    loop.close()
